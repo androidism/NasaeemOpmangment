34e4d2d477f05025dbe84b538d9f95f8b3a1bc1b
ismail96242 <90244766+androidism@users.noreply.github.com>
2021-09-15 13:58:24 +0300

    3

--- app/src/main/java/org/ismail/nasaeemopmangment/MainActivity.java
+++ app/src/main/java/org/ismail/nasaeemopmangment/MainActivity.java
@@ -1,14 +1,41 @@
 package org.ismail.nasaeemopmangment;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
 
 public class MainActivity extends AppCompatActivity {
+/*
+    EditText usertxt;
+    String usertxt2;
+    DatabaseReference myRef;
 
+ */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+    //    FirebaseDatabase database = FirebaseDatabase.getInstance();
+     //   myRef = database.getReference();
+
+
     }
+    /*
+    public void click1 (View v){
+
+        usertxt = findViewById(R.id.usertxt);
+
+        usertxt2 = (usertxt.getText().toString());
+        myRef.child("User:").setValue(usertxt2);
+
+    }
+
+     */
 }
