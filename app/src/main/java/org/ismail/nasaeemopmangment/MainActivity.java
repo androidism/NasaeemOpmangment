4b4c62a18abf79f7272c40fe0af19f5f55b7174b
ismail96242 <90244766+androidism@users.noreply.github.com>
2021-09-16 14:34:01 +0300

    5

--- app/src/main/java/org/ismail/nasaeemopmangment/MainActivity.java
+++ app/src/main/java/org/ismail/nasaeemopmangment/MainActivity.java
@@ -2,38 +2,35 @@ package org.ismail.nasaeemopmangment;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
-//import android.content.Intent;
 import android.content.Intent;
 import android.net.wifi.hotspot2.pps.HomeSp;
 import android.os.Bundle;
+import android.text.style.UpdateLayout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-/*
-//import com.google.android.ads.mediationtestsuite.activities.HomeActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.DatabaseReference;
-*/
+
+
 public class MainActivity extends AppCompatActivity {
-    /*
-        EditText usertxt, passtxt;
-        Button btnlog;
-        DatabaseReference databaseReference;
+
+        EditText usertxt1, passtxt1;
+        Button btnlog1;
+    //    DatabaseReference databaseReference;
         FirebaseAuth firebaseAuth;
         FirebaseUser firebaseUser;
+    String tag = "AnonymousAuth";
 
 
-    */
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -41,8 +38,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnlog1 =findViewById(R.id.btnlog);
+        usertxt1 =findViewById(R.id.usertxt);
+        passtxt1 =findViewById(R.id.passtxt);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btnlog1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email =usertxt1.getText().toString().trim();
+                String pass =passtxt1.getText().toString().trim();
+
+                if (!email.isEmpty()&& !pass.isEmpty())
+                {
+                    firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "Welcome TO", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                                startActivity(intent);
+                            } else
+                                {
+                                Toast.makeText(MainActivity.this, "Failed Logged", Toast.LENGTH_SHORT).show();
+                                }
+                            //      if(task.isSuccessful()){
+                            //   Log.d(tag,"Completed");
+                            //    FirebaseUser user= firebaseAuth.getCurrentUser();
+                            //    updateUI(user);
+                        }
+
+                              //  Log.w(tag, "linkWithCredential:failure", task.getException());
+                              //  Toast.makeText(MainActivity.this,"Failed!",Toast.LENGTH_SHORT).show();
+                              //  updateUI(null);
+
+                           // Toast.makeText(MainActivity.this,"Welcome!",Toast.LENGTH_SHORT).show();
+                         //   Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                           // startActivity(intent);
+
+                    });
+                }
+            }
+
+            });
+
+        }
+
     }
-}
+
 /*
         btnlog =findViewById(R.id.btnlog);
         usertxt =findViewById(R.id.usertxt);
