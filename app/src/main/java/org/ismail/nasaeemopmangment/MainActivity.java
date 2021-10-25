389e27d3275eedb7358e0f01cb68389f4f60ff00
androidism <90244766+androidism@users.noreply.github.com>
2021-10-25 23:39:05 +0300

    13

--- app/src/main/java/org/ismail/nasaeemopmangment/MainActivity.java
+++ app/src/main/java/org/ismail/nasaeemopmangment/MainActivity.java
@@ -1,60 +1,58 @@
 package org.ismail.nasaeemopmangment;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.firebase.ui.auth.data.model.User;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import org.ismail.Prevalent.Prevalent;
-
-import java.util.regex.Pattern;
+import com.google.firebase.auth.FirebaseUser;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private FirebaseAuth mAuth;
-    private Button btnlog;
-    private EditText usertxt;
-    private EditText passtxt;
-    private String parentDbName = "User";
+public class MainActivity extends AppCompatActivity {
+     FirebaseAuth mAuth;
+     FirebaseUser mUser;
+     Button btnlog;
+     EditText usertxt;
+     EditText passtxt;
+     String parentDbName = "User";
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         mAuth = FirebaseAuth.getInstance();
-
+        mUser=mAuth.getCurrentUser();
         btnlog =(Button) findViewById(R.id.btnlog);
-        btnlog.setOnClickListener(this);
-
+        progressDialog=new ProgressDialog(this);
         usertxt =(EditText) findViewById(R.id.usertxt);
         passtxt =(EditText) findViewById(R.id.passtxt);
-    }
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-        case R.id.btnlog: btnlog();
-        break;
+        btnlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perforLogin();
+            }
+
+        });
         }
 
-}
-    private void btnlog() {
+
+    private void perforLogin() {
+
      String email= usertxt.getText().toString().trim();
      String pass= passtxt.getText().toString().trim();
 
@@ -76,61 +74,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             passtxt.requestFocus();
             return;
         }
-        /*
-        mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>()
-        {
+        mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful())
+
+                if (task.isSuccessful())
                 {
-                    startActivity(new Intent(MainActivity.this  ,MainActivity2.class));
+                    progressDialog.dismiss();
+                    sendUserToNextActivity();
+                    Toast.makeText(MainActivity.this, "تم الدخول بنجاح",Toast.LENGTH_SHORT).show();
+                }else
+                {
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this,""+task.getException(),Toast.LENGTH_SHORT).show();
                 }
-                else
-                    {
-                        Toast.makeText(MainActivity.this, "Failed To Login!", Toast.LENGTH_LONG).show();
-                    }
-            }
-        });
-        */
-    }
-    private void AllowAccessToAccount(final String email, final String pass)
-    {
-        final DatabaseReference RootRef;
-        RootRef = FirebaseDatabase.getInstance().getReference();
-        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                if (dataSnapshot.child(parentDbName).child(email).exists()){
-
-                    User usersData = dataSnapshot.child(parentDbName).child(email).getValue(User.class);
-                    if (usersData.getEmail().equals(email))
-                    {
-                        if (usersData.getPhoneNumber().equals(pass))
-                        {
-                           if (parentDbName.equals("User"))
-                           {
-                                Toast.makeText(MainActivity.this, "logged in Successfully...", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                                Prevalent.currentOnlineUser = usersData;
-                                startActivity(intent);
-                            }
-                        }
-                        else
-                            {
-                            Toast.makeText(MainActivity.this,"Password is incorrect",Toast.LENGTH_SHORT).show();
-                            }
-                    }
-                }
-                else {
-                    Toast.makeText(MainActivity.this, "Account with this " + email + " number do not exists.", Toast.LENGTH_SHORT).show();
-                }
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
             }
         });
 
     }
-}
+
+    private void sendUserToNextActivity() {
+        Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    }
+
+    }
