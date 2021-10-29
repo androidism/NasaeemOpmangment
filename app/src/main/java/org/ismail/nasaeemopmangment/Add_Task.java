dec491a558e1a22fec34b979b91b815caf473472
androidism <90244766+androidism@users.noreply.github.com>
2021-10-29 15:20:19 +0200

    18

---
+++ app/src/main/java/org/ismail/nasaeemopmangment/Add_Task.java
@@ -0,0 +1,14 @@
+package org.ismail.nasaeemopmangment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Add_Task extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+    }
+}
